2d541dbb907876d143fa992c2e40a954d0011f7f
Sravan Chittupalli <sravanchittupalli7@gmail.com>
2020-12-10 13:13:53 +0530

    Added files for lab_6

--- lab_5/code/binarySearch.asm
+++ lab_5/code/binarySearch.asm
@@ -1,8 +1,8 @@
 .model small
 
 .data
-     ARR DW 0011H,2201H,3723H,4512H,5432H,7126H,0A125H,0C123H 
-     KEY DW 0A125H
+     ARR DW 0011H,2201H,3723H,4512H,5432H,7126H,0A125H,0C123H, 0D452H 
+     KEY DW 2201H
      RES DB 0FFh
      LEN DW ($-ARR)/2
 
@@ -14,7 +14,8 @@
       MOV DS,AX
     
       MOV BX,00
-      MOV DX,LEN
+      MOV DX,LEN 
+      DEC DX
       MOV CX,KEY
 AGAIN:
       CMP BX,DX
